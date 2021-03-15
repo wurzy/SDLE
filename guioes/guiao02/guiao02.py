009311e885f5d07308ed100655b251c1c05c8b18
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-03-15 22:42:07 +0000

    g02

--- guioes/guiao02/guiao02.py
+++ guioes/guiao02/guiao02.py
@@ -1,47 +1,63 @@
 import networkx as nx
 import matplotlib.pyplot as plot
-import numpy as np
 from collections import Counter
 import random
 
-minN = 10
-maxN = 100
-step = 10
-repeat = 1
+minN = 1
+maxN = 30
 
-def getRandomNodes1(graph): # { (1,3) , (2,5) } => 1,1,1,2,2,2,2,2 => random = (1,2)
+nodes = []
+edges = []
+
+def unfoldDegrees(graph):
     degrees = []
-    for (node,val) in graph.degree():
-        repeat = [node] * (val + 1)
+    for (node,val) in graph.degree(): # [ (1,2), (3,2) ] => [1, 1, 3, 3]
+        repeat = [val] * (node + 1) 
         degrees = degrees + repeat 
-    print(graph.degree())
-    nodes = random.sample(degrees,2)
-    while graph.has_edge(nodes[0],nodes[1]):
-        nodes = random.sample(degrees,2) # dont allow repeated edges; must be different nodes
-    return nodes
+    return degrees
 
-def getRandomNodes(degrees):
-    total = sum(degrees)
-    size = len(degrees)
-    result = []
-    for i in range(size):
-        result.append(degrees[i]/total) # creating probability array
-    return np.random.choice(range(0,size), p=result) # uniformly choosing nodes
+def nodesByDegree(graph):
+    x = []
+    y = []
+    degrees = [val for (node, val) in graph.degree()]
+    count = Counter(degrees).most_common() #[(1, 24), (2, 2), (4, 4)]
+    for (d,n) in count:
+        x.append(d)
+        y.append(n)
+    return x,y
 
-    
-def edgesInPreferentialGraph(n):
-    g = nx.Graph()
-    g.add_nodes_from(range(0,n))
-    degrees = [1] * n
+# returns preferred node for attachment
+def preferentialAttachment(graph,u):
+    degrees = unfoldDegrees(graph)
+    v = random.choice(degrees)
+    while graph.has_edge(u,v) or u==v: # dont allow repeated edges; must be different nodes
+        v = random.choice(degrees)
+    return v
+
+g = nx.Graph()
+for u in range(minN,maxN+1):
+    g.add_node(u)
     while not nx.is_connected(g):
-        u = random.choice(list(g.nodes()))
-        v = getRandomNodes(degrees)
-        if not g.has_edge(u, v) and not u == v:
-            g.add_edge(u, v)
-            degrees[v] += 1
-    stats = Counter(degrees).most_common()
-    print(stats)
+        v = preferentialAttachment(g,u)
+        g.add_edge(u,v)
+    nodes.append(u)
+    edges.append(nx.number_of_edges(g))
 
-for i in range(minN,maxN + step,step):
-    for j in range(repeat):
-        edges = edgesInPreferentialGraph(i)
+plot.plot(nodes,edges)
+plot.xlabel('Nodes')
+plot.ylabel('# edges')
+plot.axis('scaled')
+plot.show()
+
+x,y = nodesByDegree(g)
+
+plot.bar(x, y, align='center')
+plot.xlabel('Degree')
+plot.ylabel('# of nodes')
+plot.title('Degree Distribution')
+plot.show()
+
+plot.scatter(x,y)
+plot.yscale('log')
+plot.xscale('log')
+plot.show()

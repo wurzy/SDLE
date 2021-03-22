1f3bb56ba8139e6b67f724be4e255760f69a32ce
Válter Carvalho <47797461+wurzy@users.noreply.github.com>
2021-03-22 18:55:54 +0000

    guiao02 fixed

--- guioes/guiao02/guiao02.py
+++ guioes/guiao02/guiao02.py
@@ -1,67 +1,76 @@
 import networkx as nx
 import matplotlib.pyplot as plot
+import statistics as st
 from collections import Counter
 import random
 
-minN = 1
-maxN = 30
+minN = 10
+maxN = 100
+step = 1
+
+repeat = 30
 
 nodes = []
 edges = []
-
-def unfoldDegrees(graph):
-    degrees = []
-    for (node,val) in graph.degree(): # [ (1,2), (3,2) ] => [1, 1, 3, 3]
-        repeat = [val] * (node + 1) 
-        degrees = degrees + repeat 
-    return degrees
 
 def nodesByDegree(graph):
     x = []
     y = []
     degrees = [val for (node, val) in graph.degree()]
     count = Counter(degrees).most_common() #[(1, 24), (2, 2), (4, 4)]
+    
     for (d,n) in count:
         x.append(d)
         y.append(n)
     return x,y
 
 # returns preferred node for attachment
-def preferentialAttachment(graph,u):
-    degrees = unfoldDegrees(graph)
-    v = random.choice(degrees)
-    while graph.has_edge(u,v) or u==v: # dont allow repeated edges; must be different nodes
-        v = random.choice(degrees)
-    return v
-
-g = nx.Graph()
-for u in range(minN,maxN+1):
-    g.add_node(u)
-    while not nx.is_connected(g):
-        v = preferentialAttachment(g,u)
+def preferentialAttachment(n):
+    nodes = []
+    edges = []
+    g = nx.Graph()
+    choices = [0]
+    for u in range(1,n):
+        g.add_node(u)
+        v = random.choice(choices)
+        choices.append(u)
+        choices.append(u)
+        choices.append(v)
         g.add_edge(u,v)
-    nodes.append(u)
-    edges.append(nx.number_of_edges(g))
+    return g
 
-nx.draw(g,with_labels=True)
-plot.draw()
-plot.show()
+cur = []
+g = None
+for n in range(minN, maxN + step, step):
+    for i in range(repeat):
+        g = preferentialAttachment(n)
+        cur.append(nx.number_of_edges(g))
+    nodes.append(n)
+    edges.append(st.mean(cur))
 
+
+plot.subplot(2, 2, 1)
 plot.plot(nodes,edges)
 plot.xlabel('Nodes')
 plot.ylabel('# edges')
 plot.axis('scaled')
-plot.show()
 
 x,y = nodesByDegree(g)
 
+plot.subplot(2, 2, 2)
 plot.bar(x, y, align='center')
 plot.xlabel('Degree')
 plot.ylabel('# of nodes')
 plot.title('Degree Distribution')
-plot.show()
 
+plot.subplot(2, 2, 3)
 plot.scatter(x,y)
+plot.xlabel('(log) Degree')
+plot.ylabel('(log) # of nodes')
 plot.yscale('log')
 plot.xscale('log')
+
+plot.subplot(2,2,4)
+nx.draw(g,node_size=60,font_size=8) 
+
 plot.show()
